1c5a94aeaa5bb746d94cf4bd1c06b81c8a0b3453
hidec-7 <hiedematoaho0730127@gmail.com>
2021-06-16 15:07:00 +0900

    Create Account

--- InstagramLikeApp/Controllers/Other/Onboarding/LoginViewController.swift
+++ InstagramLikeApp/Controllers/Other/Onboarding/LoginViewController.swift
@@ -199,7 +199,36 @@ class LoginViewController: UIViewController {
         guard let usernameEmail = usernameEmailField.text, usernameEmail.isEmpty,
               let password = passwordField.text, password.isEmpty, password.count >= 8 else { return }
         
-        // login functionality
+        var username: String?
+        var email: String?
+        
+        if usernameEmail.contains("@"), usernameEmail.contains(".") {
+            // email
+            email = usernameEmail
+        }
+        else {
+            // usename
+            username = usernameEmail
+        }
+        
+        AuthManager.shared.loginUser(username: username, email: email, password: password) { success in
+            DispatchQueue.main.async {
+                if success {
+                    // user logged in
+                    self.dismiss(animated: true, completion: nil)
+                }
+                else {
+                    // error occurred
+                    let alert = UIAlertController(title: "Log In Error",
+                                                  message: "We were unable to log you in.",
+                                                  preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Dismiss",
+                                                  style: .cancel,
+                                                  handler: nil))
+                    self.present(alert, animated: true)
+                }
+            }
+        }
     }
     
     @objc private func didTapTermsButton() {
@@ -216,7 +245,8 @@ class LoginViewController: UIViewController {
     
     @objc private func didTapCreateAccountButton() {
         let vc = RegisterationViewController()
-        present(vc, animated: true)
+        vc.title = "Create Account"
+        present(UINavigationController(rootViewController: vc), animated: true)
     }
 }
 
